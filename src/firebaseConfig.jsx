aa2bcecd0402f49cc44a12d997974eb3ca6a1c78
RKabir <taposkabir@yahoo.com>
2023-10-17 00:58:04 +0600

    default mui, mui icons, redux toolkit, react-router-dom & firebase installed

---
+++ src/firebaseConfig.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBqUnU11fP5tU99Z9Ayn4cFJ6epUXv7-fI",
+  authDomain: "caged-in-fc99f.firebaseapp.com",
+  projectId: "caged-in-fc99f",
+  storageBucket: "caged-in-fc99f.appspot.com",
+  messagingSenderId: "455465213110",
+  appId: "1:455465213110:web:30856bbe57654df9839a17"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default firebaseConfig;
